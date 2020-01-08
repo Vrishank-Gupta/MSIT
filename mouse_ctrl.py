3ec3fb9c872732d644e67ff01744a7ef522b89d9
Vrishank <vrishank98@gmail.com>
2020-01-08 16:06:18 +0530

    Mouse Moving
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- mouse_ctrl.py
+++ mouse_ctrl.py
@@ -1,38 +1,175 @@
-import imutils
-import dlib
 from imutils import face_utils
 from utils import *
 import numpy as np
 import pyautogui as pag
+import imutils
+import dlib
 import cv2
 
-
-
-WINK_AR_DIFF_THRESH = 0.04
-EYE_AR_THRESH = 0.19
-WINK_CONSECUTIVE_FRAMES = 10
-WINK_AR_CLOSE_THRESH = 0.19
 MOUTH_AR_THRESH = 0.6
 MOUTH_AR_CONSECUTIVE_FRAMES = 15
+EYE_AR_THRESH = 0.19
+WINK_CONSECUTIVE_FRAMES = 10
+MOUTH_COUNTER = 0
+EYE_COUNTER = 0
+WINK_COUNTER = 0
+ANCHOR_POINT = (0, 0)
+GREEN_COLOR = (255,255, 255)
+YELLOW_COLOR = (255,255, 255)
+RED_COLOR = (255,255, 255)
+BLUE_COLOR = (255,255, 255)
+BLACK_COLOR = (255,255, 255)
+WHITE_COLOR = (255,255, 255)
 INPUT_MODE = False
 EYE_CLICK = False
 LEFT_WINK = False
 RIGHT_WINK = False
 SCROLL_MODE = False
 EYE_AR_CONSECUTIVE_FRAMES = 15
-MOUTH_COUNTER = 0
-EYE_COUNTER = 0
-WINK_COUNTER = 0
-ANCHOR_POINT = (0, 0)
-GREEN_COLOR = (0, 255, 0)
-BLUE_COLOR = (255, 0, 0)
-YELLOW_COLOR = (0, 255, 255)
-RED_COLOR = (0, 0, 255)
-BLACK_COLOR = (0, 0, 0)
-WHITE_COLOR = (255, 255, 255)
+WINK_AR_DIFF_THRESH = 0.04
+WINK_AR_CLOSE_THRESH = 0.19
 
-(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
+shape_predictor = "model/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(shape_predictor)
+
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
 (mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
+
+vid = cv2.VideoCapture(0)
+cam_w = 640
+cam_h = 480
+resolution_w = 1366
+resolution_h = 768
+unit_w = resolution_w / cam_w
+unit_h = resolution_h / cam_h
+
+while True:
+    _, frame = vid.read()
+    frame = cv2.flip(frame, 1)
+    frame = imutils.resize(frame, width=cam_w, height=cam_h)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    rects = detector(gray, 0)
+    if len(rects) > 0:
+        rect = rects[0]
+    else:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        continue
+
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+    rightEye = shape[rStart:rEnd]
+    leftEye = shape[lStart:lEnd]
+    nose = shape[nStart:nEnd]
+    mouth = shape[mStart:mEnd]
+
+    temp = leftEye
+    leftEye = rightEye
+    rightEye = temp
+
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
+    mar = mouth_aspect_ratio(mouth)
+    ear = (leftEAR + rightEAR) / 2.0
+    diff_ear = np.abs(leftEAR - rightEAR)
+
+    nose_point = (nose[3, 0], nose[3, 1])
+
+    mouthHull = cv2.convexHull(mouth)
+    leftEyeHull = cv2.convexHull(leftEye)
+    rightEyeHull = cv2.convexHull(rightEye)
+    cv2.drawContours(frame, [mouthHull], -1, YELLOW_COLOR, 1)
+    cv2.drawContours(frame, [leftEyeHull], -1, YELLOW_COLOR, 1)
+    cv2.drawContours(frame, [rightEyeHull], -1, YELLOW_COLOR, 1)
+
+    # for (x, y) in np.concatenate((mouth, leftEye, rightEye), axis=0):
+        # cv2.circle(frame, (x, y), 2, GREEN_COLOR, -1)
+        
+    if diff_ear > WINK_AR_DIFF_THRESH:
+
+        if leftEAR > rightEAR:
+            if rightEAR < EYE_AR_THRESH:
+                WINK_COUNTER += 1
+
+                if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
+                    pag.click(button='right')
+
+                    WINK_COUNTER = 0
+
+        elif leftEAR < rightEAR:
+            if leftEAR < EYE_AR_THRESH:
+                WINK_COUNTER += 1
+
+                if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
+                    pag.click(button='left')
+
+                    WINK_COUNTER = 0
+
+        else:
+            WINK_COUNTER = 0
+    else:
+        if ear <= EYE_AR_THRESH:
+            EYE_COUNTER += 1
+
+            if EYE_COUNTER > EYE_AR_CONSECUTIVE_FRAMES:
+                SCROLL_MODE = not SCROLL_MODE
+                EYE_COUNTER = 0
+
+        else:
+            EYE_COUNTER = 0
+            WINK_COUNTER = 0
+
+    if mar > MOUTH_AR_THRESH:
+        MOUTH_COUNTER += 1
+
+        if MOUTH_COUNTER >= MOUTH_AR_CONSECUTIVE_FRAMES:
+            INPUT_MODE = not INPUT_MODE
+            MOUTH_COUNTER = 0
+            ANCHOR_POINT = nose_point
+
+    else:
+        MOUTH_COUNTER = 0
+
+    if INPUT_MODE:
+        cv2.putText(frame, "on", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, BLUE_COLOR, 2)
+        x, y = ANCHOR_POINT
+        nx, ny = nose_point
+        w, h = 60, 35
+        multiple = 1
+        # cv2.rectangle(frame, (x - w, y - h), (x + w, y + h), GREEN_COLOR, 2)
+        # cv2.line(frame, ANCHOR_POINT, nose_point, BLUE_COLOR, 2)
+
+        dir = direction(nose_point, ANCHOR_POINT, w, h)
+        cv2.putText(frame,",", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
+        drag = 18
+        if dir == 'right':
+            pag.moveRel(drag, 0)
+        elif dir == 'left':
+            pag.moveRel(-drag, 0)
+        elif dir == 'up':
+            if SCROLL_MODE:
+                pag.scroll(40)
+            else:
+                pag.moveRel(0, -drag)
+        elif dir == 'down':
+            if SCROLL_MODE:
+                pag.scroll(-40)
+            else:
+                pag.moveRel(0, drag)
+
+    if SCROLL_MODE:
+        cv2.putText(frame, 'S', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
+
+    
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == 27:
+        break
+cv2.destroyAllWindows()
+vid.release()
