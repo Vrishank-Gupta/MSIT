b8649f2ccac5d0fb6fe4bb7721bd0fd07a2fa3eb
Vrishank <vrishank98@gmail.com>
2020-01-08 13:40:53 +0530

    face utils
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- mouse_ctrl.py
+++ mouse_ctrl.py
@@ -1,10 +1,11 @@
 import imutils
 import dlib
+from imutils import face_utils
+from utils import *
 import numpy as np
 import pyautogui as pag
 import cv2
-from imutils import face_utils
-from utils import *
+
 
 
 WINK_AR_DIFF_THRESH = 0.04
@@ -13,15 +14,15 @@ WINK_CONSECUTIVE_FRAMES = 10
 WINK_AR_CLOSE_THRESH = 0.19
 MOUTH_AR_THRESH = 0.6
 MOUTH_AR_CONSECUTIVE_FRAMES = 15
-EYE_AR_CONSECUTIVE_FRAMES = 15
-MOUTH_COUNTER = 0
-EYE_COUNTER = 0
-WINK_COUNTER = 0
 INPUT_MODE = False
 EYE_CLICK = False
 LEFT_WINK = False
 RIGHT_WINK = False
 SCROLL_MODE = False
+EYE_AR_CONSECUTIVE_FRAMES = 15
+MOUTH_COUNTER = 0
+EYE_COUNTER = 0
+WINK_COUNTER = 0
 ANCHOR_POINT = (0, 0)
 GREEN_COLOR = (0, 255, 0)
 BLUE_COLOR = (255, 0, 0)
@@ -29,4 +30,9 @@ YELLOW_COLOR = (0, 255, 255)
 RED_COLOR = (0, 0, 255)
 BLACK_COLOR = (0, 0, 0)
 WHITE_COLOR = (255, 255, 255)
+
+(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
 
