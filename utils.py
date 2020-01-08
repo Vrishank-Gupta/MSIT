fe4f3f18b9e225abc85ef28128e37aa754aa2fb5
Vrishank <vrishank98@gmail.com>
2020-01-08 12:40:26 +0530

    Direction
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- utils.py
+++ utils.py
@@ -1,8 +1,24 @@
 import numpy as np
 
-def eye_aspect_ratio(eye):
-	A = np.linalg.norm(eye[1] - eye[5])
-    B = np.linalg.norm(eye[2] - eye[4])
-    C = np.linalg.norm(eye[0] - eye[3])
-    ear = (A + B) / (2.0 * C)
-    return ear
+def eye_aspect_ratio(eyes):
+	A = np.linalg.norm(eyes[1] - eyes[5])
+    B = np.linalg.norm(eyes[2] - eyes[4])
+    C = np.linalg.norm(eyes[0] - eyes[3])
+    ears = (A + B) / (2.0 * C)
+    return ears
+
+def direction(nose, anchor, w, h, mult=1):
+    nx, ny = nose
+    x, y = anchor
+
+    if nx > x + mult * w:
+        return 'right'
+    elif nx < x - mult * w:
+        return 'left'
+
+    if ny > y + mult * h:
+        return 'down'
+    elif ny < y - mult * h:
+        return 'up'
+
+    return '-'
