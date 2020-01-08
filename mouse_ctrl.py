e23a3f06a4c949203eb00ca19e8668b862801d4b
Vrishank <vrishank98@gmail.com>
2020-01-08 13:17:38 +0530

    Consts
    Signed-off-by: Vrishank <vrishank98@gmail.com>

---
+++ mouse_ctrl.py
@@ -0,0 +1,32 @@
+import imutils
+import dlib
+import numpy as np
+import pyautogui as pag
+import cv2
+from imutils import face_utils
+from utils import *
+
+
+WINK_AR_DIFF_THRESH = 0.04
+EYE_AR_THRESH = 0.19
+WINK_CONSECUTIVE_FRAMES = 10
+WINK_AR_CLOSE_THRESH = 0.19
+MOUTH_AR_THRESH = 0.6
+MOUTH_AR_CONSECUTIVE_FRAMES = 15
+EYE_AR_CONSECUTIVE_FRAMES = 15
+MOUTH_COUNTER = 0
+EYE_COUNTER = 0
+WINK_COUNTER = 0
+INPUT_MODE = False
+EYE_CLICK = False
+LEFT_WINK = False
+RIGHT_WINK = False
+SCROLL_MODE = False
+ANCHOR_POINT = (0, 0)
+GREEN_COLOR = (0, 255, 0)
+BLUE_COLOR = (255, 0, 0)
+YELLOW_COLOR = (0, 255, 255)
+RED_COLOR = (0, 0, 255)
+BLACK_COLOR = (0, 0, 0)
+WHITE_COLOR = (255, 255, 255)
+
