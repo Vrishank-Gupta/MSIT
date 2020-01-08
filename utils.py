38700e9734b00be844ff271724d7b327b47c0be0
Vrishank <vrishank98@gmail.com>
2020-01-08 12:58:30 +0530

    utils resolved
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- utils.py
+++ utils.py
@@ -2,24 +2,49 @@ import numpy as np
 
 def eye_aspect_ratio(eyes):
 	A = np.linalg.norm(eyes[1] - eyes[5])
-    B = np.linalg.norm(eyes[2] - eyes[4])
-    C = np.linalg.norm(eyes[0] - eyes[3])
-    ears = (A + B) / (2.0 * C)
-    return ears
+	B = np.linalg.norm(eyes[2] - eyes[4])
+	C = np.linalg.norm(eyes[0] - eyes[3])
+
+	ears = (A + B) / (2.0 * C)
+	return ears
 
 def direction(nose, anchor, w, h, mult=1):
-    nx, ny = nose
-    x, y = anchor
+	nx, ny = nose
+	x, y = anchor
 
-    if nx > x + mult * w:
-        return 'right'
-    elif nx < x - mult * w:
-        return 'left'
+	if nx > x + mult * w:
+	    return 'right'
+	elif nx < x - mult * w:
+	    return 'left'
 
-    if ny > y + mult * h:
-        return 'down'
-    elif ny < y - mult * h:
-        return 'up'
+	if ny > y + mult * h:
+	    return 'down'
+	elif ny < y - mult * h:
+	    return 'up'
 
-    return '-'
+	return '-'
 
+def mouth_aspect_ratio(mouths):
+	A = np.linalg.norm(mouths[13] - mouths[19])
+	B = np.linalg.norm(mouths[14] - mouths[18])
+	C = np.linalg.norm(mouths[15] - mouths[17])
+	D = np.linalg.norm(mouths[12] - mouths[16])
+	mars = (A + B + C) / (2 * D)
+	return mars
+
+
+def direction(nose, anchor, w, h, mult=1):
+	nx, ny = nose
+	x, y = anchor
+
+	if ny > y + mult * h:
+	    return 'down'
+	elif ny < y - mult * h:
+	    return 'up'
+
+	if nx > x + mult * w:
+	    return 'right'
+	elif nx < x - mult * w:
+	    return 'left'
+
+	return '-'
